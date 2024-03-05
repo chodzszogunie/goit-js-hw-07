c6c2b0cd238b97dd410e35cadc911f040b4b406d
chodzszogunie <151750408+chodzszogunie@users.noreply.github.com>
2024-03-05 18:11:58 +0100

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,16 +1,25 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
-const list = document.querySelector("#ingredients");
-ingredients.forEach(ingredient => {
-const addIngredients = document.createElement("li");
-addIngredients.textContent = ingredients;
-addIngredients.classList.add('item');
-list.append(addIngredients);
-});
 
+const images = [
+  {
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
+];
+
+
+const list = document.querySelector('.gallery');
+const gallery = images.map(image => `
+  <li>
+    <img src="${image.url}" alt="${image.alt}">
+  </li>
+`).join('');
+
+gallery.insertAdjacentHTML('beforeend', gallery);
